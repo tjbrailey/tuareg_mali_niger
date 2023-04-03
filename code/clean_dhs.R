65cb60959bfbb402bb30c219690ab59716b06e26
Thomas Brailey <thomasjbrailey@gmail.com>
2023-04-03 17:22:32 +0100

    Create clean_dhs.R

---
+++ code/clean_dhs.R
@@ -0,0 +1,207 @@
+
+links_dhs <- c(
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHH01DT.zip&Tp=1&Ctry_Code=ML&surv_id=12&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR32DT.zip&Tp=1&Ctry_Code=ML&surv_id=77&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR41DT.zip&Tp=1&Ctry_Code=ML&surv_id=159&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR53DT.zip&Tp=1&Ctry_Code=ML&surv_id=276&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR61DT.zip&Tp=1&Ctry_Code=ML&surv_id=387&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR6ADT.zip&Tp=1&Ctry_Code=ML&surv_id=405&dm=1&dmode=nm", # done 
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR72DT.zip&Tp=1&Ctry_Code=ML&surv_id=487&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR7ADT.zip&Tp=1&Ctry_Code=ML&surv_id=517&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLHR81DT.zip&Tp=1&Ctry_Code=ML&surv_id=574&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR01DT.zip&Tp=1&Ctry_Code=ML&surv_id=12&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR32DT.zip&Tp=1&Ctry_Code=ML&surv_id=77&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR41DT.zip&Tp=1&Ctry_Code=ML&surv_id=159&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR53DT.zip&Tp=1&Ctry_Code=ML&surv_id=276&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR6ADT.zip&Tp=1&Ctry_Code=ML&surv_id=405&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR72DT.zip&Tp=1&Ctry_Code=ML&surv_id=487&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR7ADT.zip&Tp=1&Ctry_Code=ML&surv_id=517&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLIR81DT.zip&Tp=1&Ctry_Code=ML&surv_id=574&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLSQ31DT.zip&Tp=1&Ctry_Code=ML&surv_id=77&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLSQ42DT.zip&Tp=1&Ctry_Code=ML&surv_id=159&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLWI32DT.zip&Tp=1&Ctry_Code=ML&surv_id=77&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLWI42DT.zip&Tp=1&Ctry_Code=ML&surv_id=159&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIHR22DT.zip&Tp=1&Ctry_Code=NI&surv_id=53&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIHR31DT.zip&Tp=1&Ctry_Code=NI&surv_id=99&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIHR51DT.zip&Tp=1&Ctry_Code=NI&surv_id=277&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIHR61DT.zip&Tp=1&Ctry_Code=NI&surv_id=407&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIHR81DT.zip&Tp=1&Ctry_Code=NI&surv_id=575&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIIR22DT.zip&Tp=1&Ctry_Code=NI&surv_id=53&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIIR31DT.zip&Tp=1&Ctry_Code=NI&surv_id=99&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIIR51DT.zip&Tp=1&Ctry_Code=NI&surv_id=277&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIIR61DT.zip&Tp=1&Ctry_Code=NI&surv_id=407&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIIR81DT.zip&Tp=1&Ctry_Code=NI&surv_id=575&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NISQ21DT.zip&Tp=1&Ctry_Code=NI&surv_id=53&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NISQ31DT.zip&Tp=1&Ctry_Code=NI&surv_id=99&dm=1&dmode=nm",
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIWI31DT.zip&Tp=1&Ctry_Code=NI&surv_id=99&dm=1&dmode=nm") # done
+
+links_dhs_map <- c(
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE33FL.zip&Tp=2&Ctry_Code=ML&surv_id=77&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE42FL.zip&Tp=2&Ctry_Code=ML&surv_id=159&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE52FL.zip&Tp=2&Ctry_Code=ML&surv_id=276&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE63FL.zip&Tp=2&Ctry_Code=ML&surv_id=387&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE6BFL.zip&Tp=2&Ctry_Code=ML&surv_id=405&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE71FL.zip&Tp=2&Ctry_Code=ML&surv_id=487&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE7AFL.zip&Tp=2&Ctry_Code=ML&surv_id=517&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=MLGE81FL.zip&Tp=2&Ctry_Code=ML&surv_id=574&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIGE23FL.zip&Tp=2&Ctry_Code=NI&surv_id=53&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIGE32FL.zip&Tp=2&Ctry_Code=NI&surv_id=99&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIGE61FL.zip&Tp=2&Ctry_Code=NI&surv_id=407&dm=1&dmode=nm", # done
+  "https://www.dhsprogram.com/customcf/legacy/data/download_dataset.cfm?Filename=NIGE81FL.zip&Tp=2&Ctry_Code=NI&surv_id=575&dm=1&dmode=nm") # done
+
+
+# clean each data set 
+files_dhs_geo <- list.files(
+  here::here("data", "dhs_raw"))[stringr::str_detect(
+    string = list.files(here::here("data", "dhs_raw")), pattern = "GE")]
+
+# loop start here
+lapply(X = seq_along(files_dhs_geo), FUN = function(x){
+  
+  timer_start <- Sys.time()
+  
+  df_dhs <- sf::st_read(
+    paste0(
+      here::here(
+        "data",
+        "dhs_raw", 
+        files_dhs_geo[x]), "/", 
+      stringr::str_replace(
+        files_dhs_geo[x], "DT", "FL"), 
+      ".shp"))
+  
+  # recode dataset metadata
+  df_dhs %<>% 
+    dplyr::mutate(
+      country = dplyr::case_when(
+        substr(files_dhs_geo[x], 1, 2) == "ML" ~ "Mali",
+        substr(files_dhs_geo[x], 1, 2) == "NI" ~ "Niger",
+        TRUE ~ NA_character_),
+      survey = "Geographic",
+      round = substr(files_dhs_geo[x], 5, 5)) %>% 
+    dplyr::rename(hv001 = DHSCLUST)
+  
+  # save file
+  sf::write_sf(
+    df_dhs, paste0(
+      here::here("data", "dhs_clean"), "/", files_dhs_geo[x], "_clean.shp"))
+  
+  timer_end <- Sys.time()
+  
+  print(paste0(
+    "Cleaned data '", files_dhs_geo[x], "'."))
+  
+  print(paste0(
+    "It took about ", round(timer_end - timer_start, 1), " seconds."))
+  
+})
+
+# read in and merge all spatial data 
+files_dhs_geo_clean <- list.files(
+  path = here::here("data", "dhs_clean"), 
+  pattern = "*.shp", full.names = TRUE)
+
+files_dhs_geo_clean <- setNames(files_dhs_geo_clean, files_dhs_geo_clean)
+
+# combine
+df <- lapply(files_dhs_geo_clean, sf::read_sf) 
+
+# merge
+df <- data.table::rbindlist(df)
+
+# save 
+saveRDS(df, here::here("data", "dhs_clean", "ml_ni_spatial.rds"))
+
+###########################################################################
+##### 
+
+files_dhs <- list.files(
+  here::here("data", "dhs_raw"))[stringr::str_detect(
+    string = list.files(here::here("data", "dhs_raw")), pattern = "HR")]
+
+# loop start here
+lapply(X = seq_along(files_dhs), FUN = function(x){
+  
+  timer_start <- Sys.time()
+  
+  df_dhs <- haven::read_dta(
+    paste0(
+      here::here(
+        "data",
+        "dhs_raw", 
+        files_dhs[x]), "/", 
+      stringr::str_replace(
+        files_dhs[x], "DT", "FL"), 
+      ".dta"))
+  
+  # store labels
+  labels_df_dhs <- labelled::var_label(df_dhs)
+  
+  # remove empty columns
+  df_dhs %<>% 
+    dplyr::mutate(dplyr::across(dplyr::everything(), ~ as.numeric(.))) %>% 
+    janitor::remove_empty(., which = "cols")
+  
+  # recode dataset metadata
+  df_dhs %<>% 
+    dplyr::mutate(
+      country = dplyr::case_when(
+        substr(files_dhs[x], 1, 2) == "ML" ~ "Mali",
+        substr(files_dhs[x], 1, 2) == "NI" ~ "Niger",
+        TRUE ~ NA_character_),
+      survey = "Household Survey Recode",
+      round = substr(files_dhs[x], 5, 5)) 
+  
+  # add labels back in
+  labelled::var_label(df_dhs) <- c(as.character(
+    labels_df_dhs[names(labels_df_dhs) %in% variable.names(df_dhs)]),
+    "country", "survey type", "survey round")
+  
+  # save file
+  haven::write_dta(
+    df_dhs, paste0(
+      here::here("data", "dhs_clean"), "/", files_dhs[x], "_clean.dta"))
+  
+  timer_end <- Sys.time()
+  
+  print(paste0(
+    "Cleaned data '", files_dhs[x], "'."))
+  
+  print(paste0(
+    "It took about ", round(timer_end - timer_start, 1), " seconds."))
+  
+})
+
+# read in and merge all spatial data 
+files_dhs_clean <- list.files(
+  path = here::here("data", "dhs_clean"), 
+  pattern = "HR", full.names = TRUE)
+
+files_dhs_clean <- setNames(files_dhs_clean, files_dhs_clean)
+
+# combine
+df <- lapply(files_dhs_clean, haven::read_dta) 
+
+# merge
+vec_common_vars <- variable.names(
+  df[[1]][variable.names(df[[1]]) %in% variable.names(df[[2]])])
+vec_common_vars <- vec_common_vars[vec_common_vars %in% variable.names(df[[3]])]
+vec_common_vars <- vec_common_vars[vec_common_vars %in% variable.names(df[[4]])]
+vec_common_vars <- vec_common_vars[vec_common_vars %in% variable.names(df[[5]])]
+vec_common_vars <- vec_common_vars[vec_common_vars %in% variable.names(df[[6]])]
+
+df <- purrr::map(df, ~ .x %>% dplyr::select(dplyr::all_of(vec_common_vars)))
+
+df <- data.table::rbindlist(df)
+
+variable.names(df) %in% variable.names(df_dhs_spatial)
+
+df <- dplyr::left_join(df, dplyr::select(df_dhs_spatial, -survey))
+
+ggplot() +
+  geom_sf(data = df_shp_mali) + 
+  geom_point(data = df, mapping = aes(x = LONGNUM, y = LATNUM))
+
+# save 
+saveRDS(df, here::here("data", "dhs_clean", "ml_ni_spatial.rds"))
+
